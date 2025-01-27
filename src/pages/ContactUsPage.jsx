936a14e4e92070c2e64d037fef1f1495c4162811
zh-hadi <zhhadi50@gmail.com>
2025-01-27 08:34:55 +0600

    update

--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -19,15 +19,23 @@ const ContactUsPage = () => {
     <div className='w-3/4 mx-auto'>
       <div className='flex items-center justify-evenly'>
         <div className=' flex flex-col items-end text-right gap-5'>
-          <h2 className='text-5xl font-bold leading-normal text-transparent bg-clip-text bg-gradient-to-r from-[#0E2C46]  to-[#226CAC]'>Connect with NeuString</h2>
+          <h2 className='text-4xl leading-9 font-bold  text-transparent bg-clip-text bg-gradient-to-r from-[#0F314F]  to-[#226CAC]'>Connect with NeuString</h2>
           <p>Follow us for the latest insights, tips, and industry news.</p>
           <a href="" className='border-2 border-border-gradient  px-4 py-2 rounded-md'>Follow NeuString</a>
+          <div 
+  className="border-2 border-solid border-transparent bg-clip-border" 
+  style={{ borderImage: 'linear-gradient(78.86deg, #82BC34 6.42%, #CEDD45 96.24%)' }}
+>
+  hadi
+</div>
+
+
         </div>
         <div>
           <img className='' src={PhoneLogo} alt="" />
         </div>
         <div>
-          <h2 className='text-5xl font-bold leading-normal text-transparent bg-clip-text bg-gradient-to-r from-[#0E2C46]  to-[#226CAC]'>Download Our Mobile App</h2>
+          <h2 className='text-4xl font-bold leading-normal text-transparent bg-clip-text bg-gradient-to-r from-[#0E2C46]  to-[#226CAC]'>Download Our Mobile App</h2>
           <p>Stay connected on the go</p>
           <a href="">
             <img src={IphoneIcon} alt="" />
